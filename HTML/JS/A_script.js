06e19cc874ea964d6a5e1653980871800f6c9a77
AIP-77 <Aleksei-i-p@bk.ru>
2026-05-19 19:23:08 +0500

    Update A_script.js

--- HTML/JS/A_script.js
+++ HTML/JS/A_script.js
@@ -1,5 +1,5 @@
 // === ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ===
-import { getArchiveNameForDate, getArchiveUrl, loadData } from './data.js';
+import { getArchiveNameForDate, getArchiveUrl, loadData, getLastAvailableDate } from './data.js';
 
 let records = [];
 let standards = [];
